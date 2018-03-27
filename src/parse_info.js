502c4d75a53e12cd8e360adc0f80536c8768bf24
crichmond1989 <crichmond1989@gmail.com>
2018-03-27 13:15:32 -0500

    fixed maxBuffer error with parse_info.js expanded to allow arrays when using recursion

--- src/parse_info.js
+++ src/parse_info.js
@@ -1,26 +1,73 @@
-var exec = require('child_process').exec;
+var spawn = require('child_process').spawn;
 
-module.exports = function parse_svn_info(local_repo, next){
-  var info_call = exec('svn info ' + local_repo,
-    function(error, stdout, stderr){
-      var out_lines;
-      var extractor = /^(.*): (.*)$/;
-      result = {};
+module.exports = function (options, next) {
+  var args = ["info"];
 
-      if (error) return next(error);
+  options = options || {};
 
-      if (stdout.indexOf('\r\n') != -1) {
-        out_lines = stdout.split('\r\n');
-      } else {
-        out_lines = stdout.split('\n');
+  if (options.remote) {
+    args.push(options.remote);
+  } else if (this.remote) {
+    args.push(this.remote);
+  }
+
+  if (options.recursive)
+    args.push("-R");
+
+  if (options.revision) {
+    args.push("-r");
+    args.push(options.revision.toString());
+  }
+
+  var child = spawn('svn', args, {
+    cwd: this.local
+  });
+
+  var stdout = '';
+  var stderr = '';
+
+  child.stdout.on('data', function (data) {
+    stdout += data;
+  });
+
+  child.stderr.on('data', function (data) {
+    stderr += data;
+  });
+
+  child.on('close', function (code) {
+    if (code !== 0)
+      return next(new Error(stderr));
+
+    var out_lines;
+    var extractor = /^(.*): (.*)$/;
+    var result = {};
+    var results = [];
+
+    if (stdout.indexOf('\r\n') != -1) {
+      out_lines = stdout.split('\r\n');
+    } else {
+      out_lines = stdout.split('\n');
+    }
+
+    out_lines.forEach(function (line) {
+      var match = extractor.exec(line);
+
+      if (!match)
+        return;
+
+      if (match[1] in result) {
+        results.push(result);
+        result = {};
       }
 
-      out_lines.forEach(function(line){
-        var match = extractor.exec(line);
-        if( match ){
-          result[match[1]] = match[2];
-        }
-      });
-      next(null, result);
+      result[match[1]] = match[2];
     });
+
+    results.push(result);
+
+    if (results.length > 1)
+      return next(null, results);
+
+    return next(null, result);
+  });
 };
--- src/svn.js
+++ src/svn.js
@@ -1,25 +1,19 @@
 var path = require('path');
 
-var SVN = module.exports = function SVN(local, remote){
+var SVN = module.exports = function SVN(local, remote) {
   this.local = path.resolve(local);
   this.remote = remote;
 }
 
-SVN.prototype.get_remote = function get_remote(next){
+SVN.prototype.get_remote = function get_remote(next) {
   var parse_info = require('./parse_info.js');
-  parse_info(this.local, function(err, result){
+  parse_info(this.local, function (err, result) {
     if (err) return next(err);
     next(null, result['Repository Root']);
   })
 }
 
-SVN.prototype.info = function info(url, next){
-  var parse_info = require('./parse_info.js');
-  parse_info(url, function(err, result){
-    if (err) return next(err);
-    next(null, result);
-  })
-}
+SVN.prototype.info = require("./parse_info.js");
 
 SVN.prototype.externals = require('./externals.js');
 
