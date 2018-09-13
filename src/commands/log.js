47c3b0cd91566e845d133a5d4c993e01afa536c0
Christopher Richmond <crichmond1989@gmail.com>
2018-09-13 01:50:17 -0500

    updated license to include original copyright added moment removed log_old split out parseArgs added tests for log

--- src/commands/log.js
+++ src/commands/log.js
@@ -1,3 +1,5 @@
+import moment from "moment";
+
 import parseXml from "../parseXml.js";
 import spawn from "../spawn";
 
@@ -8,32 +10,69 @@ export default class {
         if (!options.source)
             throw new Error("source is required");
 
+        if (options.revision && (options.start || options.end))
+            throw new Error("must choose between revision or using start and/or end");
+
         this.options = options;
     }
 
     async exec() {
-        const args = ["log", "--xml"];
-
-        if (this.options.limit)
-            args.push(...["-l", this.options.limit.toString()]);
-
-        if (this.options.paths)
-            args.push("-v");
-
-        args.push(this.options.source);
+        const args = this.parseArgs();
 
         const xml = await spawn("svn", args);
 
         return await this.transform(xml);
     }
 
+    parseArgs() {
+        const args = ["log", "--xml"];
+
+        if (this.options.limit)
+            args.push(...["-l", this.options.limit.toString()]);
+
+        if (this.options.revision)
+            args.push(...["-r", this.options.revision.toString()]);
+
+        if (this.options.start || this.options.end) {
+            args.push("-r");
+
+            let formattedStart = "1";
+
+            if (this.options.start) {
+                const startMt = moment.utc(this.options.start, moment.ISO_8601, true);
+
+                formattedStart = startMt.isValid() ? `{${startMt.format("YYYY-MM-DD")}}` : "1";
+            }
+
+            let formattedEnd = "HEAD";
+
+            if (this.options.end) {
+                const endMt = moment.utc(this.options.end, moment.ISO_8601, true);
+
+                formattedEnd = endMt.isValid() ? `{${endMt.format("YYYY-MM-DD")}}` : "HEAD";
+            }
+
+            args.push(`${formattedStart}:${formattedEnd}`);
+        }
+
+        if (this.options.paths)
+            args.push("-v");
+
+        args.push(this.options.source);
+
+        return args;
+    }
+
     async transform(xml) {
         const obj = await parseXml(xml);
 
+        if (!obj.log.logentry)
+            return [];
+
         return obj.log.logentry.map(x => ({
-            revision: x.$.revision,
+            revision: parseInt(x.$.revision, 10),
             author: x.author[0],
-            date: x.date[0],
+            date: moment(x.date[0]),
             message: x.msg[0],
             paths: x.paths && x.paths[0].path.map(y => y._)
         }));
--- src/commands/log.test.js
+++ src/commands/log.test.js
@@ -1,7 +1,45 @@
+import moment from "moment";
+
 import log from "./log.js";
 
 const svnLocal = __dirname + "/../../resources/fake-svn-project";
 const svnUrl = "https://svn.code.sf.net/p/svnbook/source";
+
+it("args: default start to 1", () => {
+    const result = new log({ source: svnLocal, end: "20180913" }).parseArgs();
+
+    expect(result).toContain("1:{2018-09-13}");
+})
+
+it("args: default end to HEAD", () => {
+    const result = new log({ source: svnLocal, start: "20180913" }).parseArgs();
+
+    expect(result).toContain("{2018-09-13}:HEAD");
+})
+
+it("args: Date object", () => {
+    const result = new log({ source: svnLocal, start: new Date("2018-09-13") }).parseArgs();
+
+    expect(result).toContain("{2018-09-13}:HEAD");
+})
+
+it("args: moment object", () => {
+    const result = new log({ source: svnLocal, start: moment("2018-09-13") }).parseArgs();
+
+    expect(result).toContain("{2018-09-13}:HEAD");
+})
+
+it("args: respect timezone", () => {
+    const result = new log({ source: svnLocal, start: moment("2018-09-13T00:00:00+0500") }).parseArgs();
+
+    expect(result).toContain("{2018-09-12}:HEAD");
+})
+
+it("args: paths adds verbosity flag", () => {
+    const result = new log({ source: svnLocal, paths: true }).parseArgs();
+
+    expect(result).toContain("-v");
+})
 
 it("uses: local svn", async () => {
     const result = await new log({ source: svnLocal, limit: 1 }).exec();
@@ -13,6 +51,27 @@ it("uses: remote svn", async () => {
     const result = await new log({ source: svnUrl, limit: 1 }).exec();
 
     expect(result).toHaveLength(1);
+})
+
+it("uses: revision number", async () => {
+    const revision = 5615;
+    const result = await new log({ source: svnLocal, limit: 1, revision }).exec();
+
+    expect(result[0].revision).toBe(revision);
+})
+
+it("uses: start", async () => {
+    const start = "2018-01-26";
+    const result = await new log({ source: svnLocal, limit: 1, start }).exec();
+
+    expect(result[0].date.valueOf()).toBeGreaterThanOrEqual(moment(start).valueOf());
+})
+
+it("uses: end", async () => {
+    const end = "2018-01-26";
+    const result = await new log({ source: svnLocal, limit: 1, end }).exec();
+
+    expect(result[0].date.valueOf()).toBeLessThanOrEqual(moment(end).valueOf());
 })
 
 it("returns: 1 result without path", async () => {
@@ -37,17 +96,26 @@ it("returns: 2 results", async () => {
 
 it("throws: source is required", async () => {
     try {
-        await new log().exec();
-        fail("expected a missing argument error");
+        new log();
+        fail();
     } catch (error) {
         expect(error.message).toBe("source is required");
+    }
+})
+
+it("throws: choose between revision or using start and/or end", () => {
+    try {
+        new log({ source: svnLocal, revision: 1, start: "20180913" });
+        fail();
+    } catch (error) {
+        expect(error.message).toBe("must choose between revision or using start and/or end");
     }
 })
 
 it("throws: is not a working copy", async () => {
     try {
         await new log({ source: svnLocal + "nope" }).exec();
-        fail("expected a connection error");
+        fail();
     } catch (error) {
         expect(error.message).toMatch(/is not a working copy/);
     }
@@ -56,7 +124,7 @@ it("throws: is not a working copy", async () => {
 it("throws: unable to connect to a repository", async () => {
     try {
         await new log({ source: "https://nope.nope" }).exec();
-        fail("expected a connection error");
+        fail();
     } catch (error) {
         expect(error.message).toMatch(/Unable to connect to a repository/);
     }
