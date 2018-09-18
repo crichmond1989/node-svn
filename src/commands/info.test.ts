61b193f7e9549a145699f9c78ab5b608993c0104
crichmond1989 <crichmond1989@gmail.com>
2018-09-18 14:11:26 -0500

    circleci: add test reporting jest: add jest-junit

--- src/commands/info.test.ts
+++ src/commands/info.test.ts
@@ -6,15 +6,17 @@ const svnUrl = Config.testSvnUrl;
 
 const getRequest = () => ({ source: svnLocal, format: "json" });
 
-it("uses: recursive", async () => {
-    const result = await new Info({ ...getRequest(), recursive: true }).exec();
+describe("uses", () => {
+    it("recursive", async () => {
+        const result = await new Info({ ...getRequest(), recursive: true }).exec();
 
-    expect(result).toHaveLength(4);
-})
+        expect(result).toHaveLength(4);
+    })
 
-it("uses: revision number", async () => {
-    const revision = 5615;
-    const result = await new Info({ ...getRequest(), revision }).exec();
+    it("revision number", async () => {
+        const revision = 5615;
+        const result = await new Info({ ...getRequest(), revision }).exec();
 
-    expect(result[0].revision).toBe(revision);
+        expect(result[0].revision).toBe(revision);
+    })
 })
